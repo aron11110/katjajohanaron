13c9e9e9c82d318d5afcce8cb1cd6eaa48cc82ad
Aron <aron.liiv@hotmail.com>
2022-10-26 18:45:01 +0300

    pildid

--- source/script.js
+++ source/script.js
@@ -10,6 +10,7 @@ function posts(json) {
         let postText = document.createElement('p');
         postText.innerText = post.text;
         let postPicture = document.createElement('img');
+        postPicture.src = "./media/postMedia/img1.jpeg";
         postPicture.width = '300';
         postPicture.height = '300';
         div.appendChild(postAuthor);
