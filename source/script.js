f645a61e87c0d072c7c5e3a24d9a4e574cfb7eeb
Aron <aron.liiv@hotmail.com>
2022-10-26 20:02:36 +0300

    hkjhas

--- source/script.js
+++ source/script.js
@@ -4,8 +4,9 @@ function posts(json) {
         let post=json[i];
         let div = document.createElement('div');
         let postTime = document.createElement('h3');
-        let rightTime = Date(post.time)
-        postTime.innerText = rightTime.toString();
+        let d = Date(post.time)
+        var date_format_str = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
+        postTime.innerText = date_format_str
         let postAuthor = document.createElement('p');
         postAuthor.innerText = post.author;
         let postText = document.createElement('p');
