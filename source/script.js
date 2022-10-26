da2d980bf878a00855603bc5fc4b393f8922acb9
Aron <aron.liiv@hotmail.com>
2022-10-26 17:00:22 +0300

    commit

--- source/script.js
+++ source/script.js
@@ -1,4 +1,12 @@
+function posts(json) {
+    for (let i=0; i < json.length; i++) {
+        
+    }
+}
+
 window.onload = function() {
     let divs = document.getElementsByTagName("div")
     console.log(divs)
+
+    fetch()
 }
