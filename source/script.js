f7d453207513f81a110b965c21c2ea841aa4e8f8
Johan Kirikal <johankir@ut.ee>
2022-10-30 22:32:05 +0200

    looks more now like the previous hw

--- source/script.js
+++ source/script.js
@@ -2,22 +2,53 @@ function posts(json) {
     let post_div = document.getElementById("posts_div")
     for (let i=0; i < json.length; i++) {
         let post=json[i];
+
+        let article = document.createElement('article')
+
+        let header = document.createElement('header')
+        let span1 = document.createElement('span')
+        let posterPhoto = document.createElement('img')
+        posterPhoto.src = './media/profileMedia/poster1.jpg'
+        posterPhoto.width = '30';
+        posterPhoto.height = '30';
+        let span2 = document.createElement('span')
+        let span3 = document.createElement('span')
+        let span4 = document.createElement('span')
+        span4.id = 'posterSpan'
         let div = document.createElement('div');
-        let postTime = document.createElement('h3');
+        let postTime = document.createElement('h5');
         postTime.innerText = datetime(post.time);
-        let postAuthor = document.createElement('p');
+        let postAuthor = document.createElement('h5');
         postAuthor.innerText = post.author;
+
+        span1.appendChild(posterPhoto)
+        span2.appendChild(postAuthor)
+        span3.appendChild(postTime)
+        span4.appendChild(span1)
+        span4.appendChild(span2)
+
+
+        header.appendChild(span4)
+        header.appendChild(span3)
+
+        let postImage = document.createElement('div')
+        postImage.className = 'postImage'
+
+        let textPart = document.createElement('div')
+        textPart.className = 'postText'
+
         let postText = document.createElement('p');
         postText.innerText = post.text;
+        textPart.appendChild(postText)
         let postPicture = document.createElement('img');
         postPicture.id= 'postedPost'
         postPicture.src = post.picture
         postPicture.width = '200';
         postPicture.height = '200';
-        div.appendChild(postAuthor);
-        div.appendChild(postTime);
-        div.appendChild(postText);
-        div.appendChild(postPicture);
+        article.appendChild(header);
+        postImage.appendChild(postPicture);
+        article.appendChild(postImage)
+        article.appendChild(textPart)
         
         if(i%2==0){
             div.style.backgroundColor='#F0FFFF'
@@ -25,27 +56,24 @@ function posts(json) {
         else{
             div.style.backgroundColor='#FF7F50'
         }
-        post_div.appendChild(div);
+        post_div.appendChild(article);
     }
 }
 
 function popup(){
     let userInfo={name:'Driscoll Jaquelyn', email:'Driscoll.Jaquelyn@mail.mail'}
     let photoParent = document.getElementById('profile')
-    console.log(photoParent)
     let infoText = ''
     infoText += userInfo.name + '\n' + userInfo.email + '\nlogout'
     let info = document.createTextNode(infoText)
     let photo = document.getElementById('profileInfo')
     photo.appendChild(info)
     photoParent.onclick = function(){
-        console.log('yes')
         if (photo.style.display === "none") {
             photo.style.display = "block";
           } else {
             photo.style.display = "none";
           }
-        console.log(photo.style.visibility)
     }
 }
 
@@ -59,7 +87,6 @@ function datetime(universal) {
 window.onload = function() {
     popup()
     let divs = document.getElementsByTagName("div")
-    console.log(divs)
     fetch("./data/data.json")
     // fetch("https://api.npoint.io/5d82e9be4d319f1563e1")
         .then(res => res.json())
